fe6a6fda20920aec24c252f0a70624f607316d9b
Jack Dermody <Jack.Dermody@dkinnovation.com>
2022-08-17 16:51:05 +0100

    Update Scrapper.py
    Added away to print out the first 1000 characters and create a html file that copies the page that is being scrap

--- Scrapper.py
+++ Scrapper.py
@@ -1,1 +1,12 @@
-print('Hello World')
+from urllib import response
+import requests
+
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
+response = requests.get(YOUTUBE_TRENDING_URL)
+
+print('Status Code', response.status_code)
+print('Output', response.text[:1000])
+
+with open('trending.html', 'w') as f:
+    f.write(response.text)
