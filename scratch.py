16f73ddb6da06edc62b75fc863df6d73af7a4c06
Jack Dermody <Jack.Dermody@dkinnovation.com>
2022-08-17 23:47:34 +0100

    Create scratch.py
    Create a placeholder for worked done

---
+++ scratch.py
@@ -0,0 +1,22 @@
+from urllib import response
+import requests
+from bs4 import BeautifulSoup
+
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
+response = requests.get(YOUTUBE_TRENDING_URL)
+
+print('Status Code', response.status_code)
+print('Output', response.text[:1000])
+
+with open('trending.html', 'w') as f:
+    f.write(response.text)
+
+doc = BeautifulSoup(response.text, 'html.parser')
+
+print('Page title:', doc.title.text)
+
+# Find all the video divs
+video_divs = doc.find_all('div', class_='ytd-video-renderer')
+
+print(f'Found {len(video_divs   )} videos')
