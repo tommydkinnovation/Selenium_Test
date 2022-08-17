638f7bbfc391effd8690cb539e5619d2fb33be64
Jack Dermody <Jack.Dermody@dkinnovation.com>
2022-08-17 23:46:26 +0100

    Update Scrapper.py
    Brought back the amount of videos found

--- Scrapper.py
+++ Scrapper.py
@@ -1,5 +1,6 @@
 from urllib import response
 import requests
+from bs4 import BeautifulSoup
 
 YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
 
@@ -10,3 +11,12 @@ print('Output', response.text[:1000])
 
 with open('trending.html', 'w') as f:
     f.write(response.text)
+
+doc = BeautifulSoup(response.text, 'html.parser')
+
+print('Page title:', doc.title.text)
+
+# Find all the video divs
+video_divs = doc.find_all('div', class_='ytd-video-renderer')
+
+print(f'Found {len(video_divs   )} videos')
